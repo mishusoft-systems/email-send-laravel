3d37872cc2113d683e38a4f8cbaa13bdc7341e1e
Al Amin Ahamed <34349365+mralaminahamed@users.noreply.github.com>
2022-07-19 08:51:46 +0600

    Initial commit

---
+++ database/migrations/2022_07_19_022019_create_sent_emails_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sent_emails', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user');
+            $table->string('first_sent_date');
+            $table->string('last_sent_date');
+            $table->string('is_unsubscribe');
+            $table->string('is_not_respond');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sent_emails');
+    }
+};
